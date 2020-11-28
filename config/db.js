6def5f53a2783f83cdfd692f9858c9c77e893221
Luis Colorado <lecdmx@gmail.com>
2020-11-28 11:42:22 -0600

    Constantes de entorno, conexion a proveedores(suppliers)

--- config/db.js
+++ config/db.js
@@ -1,14 +1,26 @@
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("DBone", "sa", "AEGqK6Sb40m", {
-  host: "localhost",
-  dialect: "mssql",   
-  port: 1434, 
-  dialectOptions: {    
-    options: {
-      validateBulkLoadParameters: true,
+
+console.log('dirname db = ', __dirname);
+console.log('env  db = ' , __dirname + '/.env');
+console.log('PORT = ' , process.env.PORT);
+
+const db = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USERNAME,
+  process.env.DATABASE_PASSWORD,
+  
+  {
+    host:  process.env.DATABASE_HOST,
+    dialect: process.env.DATABASE_DIALECT,
+    // port: 1434,
+    dialectOptions: {
+      options: {
+        database: process.env.DATABASE_NAME,
+        validateBulkLoadParameters: true,
+      },
     },
-  },
-});
+  }
+);
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var env = require('node-env-file'); // .env file
+env(__dirname + '/.env');
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const db = require("./config/db");
@@ -7,11 +11,17 @@ const router = require("./routes");
 const dbConection = async () => {
   try {
     await db.authenticate();
-    console.log("Conecction has been established successfully");
+    console.log("Conection has been established successfully");
   } catch (error) {
     console.log(error);
   }
 };
+
+console.log('dirname = ', __dirname);
+console.log('env = ' , __dirname + '/.env');
+
+const port = process.env.PORT || 3000;
+console.log('PORT = ' , process.env.PORT);
 
 dbConection();
 
@@ -21,4 +31,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(router);
-app.listen(3001);
+app.listen(port);
